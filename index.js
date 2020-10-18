a8ba95d350a7c9eee9d0db004409be66cce5ec63
LeoGonzaga <sacredlotus05@gmail.com>
2020-10-18 18:07:25 -0300

    a

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
@@ -16,6 +16,7 @@ mongoose.connect(
 );
 
 app.use(express.json());
+app.use(cors);
 app.use(routes);
 
 app.listen(port, () => {
